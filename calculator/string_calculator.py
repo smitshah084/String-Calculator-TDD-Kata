7b954dcf3305ec69666334068c267ff791d372f2
Smit Shah <smitshah084@gmail.com>
2025-07-05 15:53:44 +0530

    [STEP-3] Passed test for input with \n as delimiter

--- calculator/string_calculator.py
+++ calculator/string_calculator.py
@@ -2,7 +2,11 @@ def add(numbers: str) -> int:
     if len(numbers) == 0:
         return 0
     
-    numbers = numbers.split(',')
+    if ',' in numbers:
+        numbers = numbers.split(',')
+    elif '\n' in numbers:
+        numbers = numbers.split('\n')
+        
     numbers = [int(i) for i in numbers]
     
     return sum(numbers)
