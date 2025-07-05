214f4e83031783f498df23aef018b55d255ab053
Smit Shah <smitshah084@gmail.com>
2025-07-05 15:20:24 +0530

    [STEP-1] Failing test for empty string input

---
+++ test/test_string_calculator.py
@@ -0,0 +1,7 @@
+import unittest
+from calculator import add
+
+class TestStringCalculator(unittest.TestCase):
+    
+    def test_empty_string_returns_zero(self):
+        self.assertEqual(add(""), 0)
