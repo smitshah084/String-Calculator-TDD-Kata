685bf685e497f12b59e0db597c401266ee760234
Smit Shah <smitshah084@gmail.com>
2025-07-05 16:10:10 +0530

    [STEP-4] passed test for custom delimiters

--- calculator/string_calculator.py
+++ calculator/string_calculator.py
@@ -2,10 +2,16 @@ def add(numbers: str) -> int:
     if len(numbers) == 0:
         return 0
     
-    if ',' in numbers:
-        numbers = numbers.split(',')
-    elif '\n' in numbers:
-        numbers = numbers.split('\n')
+    if numbers[:2] == "//" and numbers[3] == '\n':
+        # custom delimiter is defined
+        delimiter = numbers[2]
+        numbers = numbers[4:]
+        numbers = numbers.split(delimiter)
+    else:
+        if ',' in numbers:
+            numbers = numbers.split(',')
+        elif '\n' in numbers:
+            numbers = numbers.split('\n')
         
     numbers = [int(i) for i in numbers]
     
--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 import unittest
 
+# Discover and run all tests in the current directory
+if __name__ == "__main__":
+    test_loader = unittest.TestLoader()
+    test_suite = test_loader.discover(start_dir=".", pattern="test*.py")
+
+    test_runner = unittest.TextTestRunner(verbosity=2)
+    test_runner.run(test_suite)
