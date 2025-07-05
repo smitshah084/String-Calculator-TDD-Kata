01b6e25f31deba70f47711a68ecca55f3d336a4a
Smit Shah <smitshah084@gmail.com>
2025-07-05 15:28:34 +0530

    [STEP-1] Failing test for 1 number input

--- test/test_string_calculator.py
+++ test/test_string_calculator.py
@@ -6,4 +6,5 @@ class TestStringCalculator(unittest.TestCase):
     def test_empty_string_returns_zero(self):
         self.assertEqual(add(""), 0)
         
-    
+    def test_one_number_input(self):
+        self.assertEqual(add("1"),1)
